7f52466a883fb9f09c9c040ac3d1c811589ad74f
yinzhidong <echoyin@tom.com>
2023-08-16 07:14:03 +0000

    fix: fix docker build image

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,3 +1,5 @@
+npm install
+npm run build-prod
 # first time, build image and run it, next time just docker run
 docker build -t echo/ai:open-pose-ui .
 
@@ -5,12 +7,11 @@ docker build -t echo/ai:open-pose-ui .
 # -p 2222:1111: The -p flag is used to map a host port (2222) to a container port (1111). This allows you to access the application running inside the container on port 1111 through port 2222 on the host machine.
 # second time, just run below, image will be pulled, then go to http://localhost:2222
 
-# docker push weialexchen/dashboardui:main-dashboard
-# docker run --name openpose-ui -dp 3100:3000 echo/ai:open-pose-ui
+docker run --name openpose-ui -dp 3000:80 echo/ai:open-pose-ui
 
 
 # docker swarm
-# docker service create -dp 3000:3000 \
-#     --name openpose-ui \
-#     --replicas 4 \
-#     echo/ai:open-pose-ui
+docker service create -dp 3000:80 \
+    --name openpose-ui \
+    --replicas 4 \
+    echo/ai:open-pose-ui
